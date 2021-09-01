d59537760fcdd5084fac3e20eba086d7ea9eec7a
janicecaldeira <janicecaldeira@gmail.com>
2021-08-31 23:02:33 -0300

    Criando Projeto

---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require("express");
+const mongodb = require("mongodb");
+const ObjectId = mongodb.ObjectId;
+require("dotenv").config();
+
+(async () => {
+	const dbUser = process.env.DB_USER;
+	const dbPassword = process.env.DB_PASSWORD;
+	const dbName = process.env.DB_NAME;
+
+	const app = express();
+	app.use(express.json());
+
+	const port = process.env.PORT || 3000;
+	const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.nlzlu.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+	const options = {
+		useUnifiedTopology: true,
+	};
+
+	const client = await mongodb.MongoClient.connect(connectionString, options);
+
+	const db = client.db("db_rickymorty");
+	const personagens = db.collection("personagens");
+
+	const getPersonagensValidas = () => personagens.find({}).toArray();
+
+	const getPersonagemById = async (id) =>
+		personagens.findOne({ _id: ObjectId(id) });
+
+	//CORS
+
+	app.all("/*", (req, res, next) => {
+		res.header("Access-Control-Allow-Origin", "*");
+
+		res.header("Access-Control-Allow-Methods", "*");
+
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+		);
+
+		next();
+	});
+
+	app.get("/", (req, res) => {
+		res.send({ info: "Olá, Blue" });
+	});
+
+	//[GET] GetAllPersonagens
+
+	app.get("/personagens", async (req, res) => {
+		res.send(await getPersonagensValidas());
+	});
+
+	//[GET] getPersonagemById
+
+	app.get("/personagens/:id", async (req, res) => {
+		const id = req.params.id;
+		const personagem = await getPersonagemById(id);
+		res.send(personagem);
+	});
+
+	app.post("/personagens", async (req, res) => {
+		const objeto = req.body;
+
+		if (!objeto || !objeto.nome || !objeto.imagemUrl) {
+			res.send(
+				"Requisição inválida, certifique-se que tenha os campos nome e imagemUrl"
+			);
+			return;
+		}
+
+		const insertCount = await personagens.insertOne(objeto);
+
+		if (!insertCount) {
+			res.send("Ocorreu um erro");
+			return;
+		}
+
+		res.send(objeto);
+	});
+
+	//[PUT] Atualizar personagem
+	app.put("/personagens/:id", async (req, res) => {
+		const id = req.params.id;
+		const objeto = req.body;
+		res.send(
+			await personagens.updateOne(
+				{
+					_id: ObjectId(id),
+				},
+				{
+					$set: objeto,
+				}
+			)
+		);
+	});
+
+	//[DELETE] Deleta um personagem
+	app.delete("/personagens/:id", async (req, res) => {
+		const id = req.params.id;
+
+		res.send(
+			await personagens.deleteOne({
+				_id: ObjectId(id),
+			})
+		);
+	});
+
+	app.listen(port, () => {
+		console.info(`App rodando em http://localhost:${port}`);
+	});
+})();
