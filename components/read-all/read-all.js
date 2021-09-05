cc6247cb33a369b5ab20b7793cd240e7ab4bcc55
janicecaldeira <janicecaldeira@gmail.com>
2021-09-05 15:46:29 -0300

    Criando rota read all

--- components/read-all/read-all.js
+++ components/read-all/read-all.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const mongodb = require("mongodb");
 
-router.use(function (req, res, next) {
+(async () => {
+  const dbUser = process.env.DB_USER;
+  const dbPassword = process.env.DB_PASSWORD;
+  const dbName = process.env.DB_NAME;
+  const dbChar = process.env.DB_CHAR;
+
+  const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+  const options = {
+    useUnifiedTopology: true,
+  };
+
+  const client = await mongodb.MongoClient.connect(connectionString, options);
+
+  const db = client.db("db_rickymorty");
+  const personagens = db.collection("personagens");
+
+  const getPersonagensValidas = () => personagens.find({}).toArray();
+
+  router.use(function (req, res, next) {
     next();
-});
+  });
 
-router.get("/personagens", async (req, res) => {
+  router.get("/", async (req, res) => {
     res.send(await getPersonagensValidas());
-});
+  });
+})();
 
 module.exports = router;
