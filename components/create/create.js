616276806e3f0aebf7768d3f39b7db15ebf65b64
janicecaldeira <janicecaldeira@gmail.com>
2021-09-05 15:46:11 -0300

    Criando rota create

---
+++ components/create/create.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const mongodb = require("mongodb");
+const ObjectId = mongodb.ObjectId;
+
+(async () => {
+  const dbUser = process.env.DB_USER;
+  const dbPassword = process.env.DB_PASSWORD;
+  const dbName = process.env.DB_NAME;
+  const dbChar = process.env.DB_CHAR;
+
+  const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+  const options = {
+    useUnifiedTopology: true,
+  };
+
+  const client = await mongodb.MongoClient.connect(connectionString, options);
+
+  const db = client.db("db_rickymorty");
+  const personagens = db.collection("personagens");
+
+  router.use(function (req, res, next) {
+    next();
+  });
+
+  router.use("/", async (req, res) => {
+    const objeto = req.body;
+
+    if (!objeto || !objeto.nome || !objeto.imagemUrl) {
+      res.status(400).send({
+        error: "Requisição inválida, obrigatório os campos nome e imagemUrl",
+      });
+      return;
+    }
+
+    const result = await personagens.insertOne(objeto);
+
+    if (result.acknowledged == false) {
+      res.status(500).send({ error: "Ocorreu um erro" });
+      return;
+    }
+
+    res.status(201).send(objeto);
+  });
+})();
+
+module.exports = router;
